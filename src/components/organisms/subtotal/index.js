1c9743b678589d5248d2d48749dcba3397f5ca4c
jiro <j.code.public@gmail.com>
2022-08-01 15:37:32 -0700

    Add payment screen

---
+++ src/components/organisms/subtotal/index.js
@@ -0,0 +1,35 @@
+import styles from "./index.module.css";
+import classNames from "classnames";
+
+export default function Subtotal({ summary }) {
+  return (
+    <div className={styles.container}>
+      {summary.map((item) => {
+        return (
+          <div className={styles.summary}>
+            <div
+              className={classNames({
+                [styles.label]: true,
+                [styles.summarySmall]: item.group === "small",
+                [styles.summaryMeidum]: item.group === "medium",
+                [styles.summaryLarge]: item.group === "large",
+              })}
+            >
+              {item.label}
+            </div>
+            <div
+              className={classNames({
+                [styles.label]: true,
+                [styles.summarySmall]: item.group === "small",
+                [styles.summaryMeidum]: item.group === "medium",
+                [styles.summaryLarge]: item.group === "large",
+              })}
+            >
+              {item.value}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}

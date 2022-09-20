a414691696b0c0a696b1bb46e4657d9ce48d01b0
Jiro <j.onodera26@gmail.com>
2022-09-19 17:55:18 -0700

    Add accounts page

--- src/components/organisms/dropdown/index.js
+++ src/components/organisms/dropdown/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import classNames from "classnames";
 import style from "./index.module.css";

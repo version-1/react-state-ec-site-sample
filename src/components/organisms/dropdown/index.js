95ecc96ef31a777ccbbd4eeaaf2c40738a1706a9
jiro <j.code.public@gmail.com>
2022-09-19 17:38:20 -0700

    Add dropdown to nav bar

---
+++ src/components/organisms/dropdown/index.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
+import style from "./index.module.css";
+
+const disruptiveStyle = "disruptive";
+
+const DropDownMenu = ({ trigger, items }) => {
+  const [show, setShow] = useState();
+
+  return (
+    <div className={style.wrapper} onMouseLeave={() => setShow(false)}>
+      <div onMouseEnter={() => setShow(true)}>{trigger}</div>
+      <ul
+        className={classNames({
+          [style.container]: true,
+          [style.show]: show,
+        })}
+      >
+        {items.map((item) => {
+          if (item.onClick) {
+            return (
+              <li
+                key={item.key}
+                onClick={() => {
+                  setShow(false);
+                  item.onClick();
+                }}
+                className={classNames({
+                  [style.item]: true,
+                  [style.disruptive]: item.style === disruptiveStyle,
+                })}
+              >
+                {item.label}
+              </li>
+            );
+          }
+
+          return (
+            <Link to={item.to}>
+              <li
+                key={item.key}
+                className={classNames({
+                  [style.item]: true,
+                  [style.disruptive]: item.style === disruptiveStyle,
+                })}
+              >
+                {item.label}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default DropDownMenu;

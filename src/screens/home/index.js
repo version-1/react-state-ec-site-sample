5942f8eb7959fc2475e366d63665ddbbe688f937
jiro <j.code.public@gmail.com>
2022-10-25 18:58:50 -0700

    Impl top page

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,18 +1,44 @@
-import Layout from "components/templates/layout";
-import Products from "components/templates/products";
+import { useCallback, useEffect, useState } from "react";
 import Slide from "components/organisms/slide";
 import icon from "assets/logo-light.png";
 import { ReactComponent as Arrow } from "assets/arrow.svg";
 import styles from "./index.module.css";
 
-const Home = ({ user }) => {
+const title = [
+  { label: "Women", path: "/#/women" },
+  { label: "Men", path: "/#/men" },
+  { label: "Kids", path: "/#/kids" },
+];
+
+const Home = () => {
+  const [index, setIndex] = useState(0);
+
+  const transit = useCallback(
+    (delta) => () => setIndex(Math.abs((index + delta) % 3)),
+    [index, setIndex]
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      transit(1)();
+    }, 5000);
+  }, [transit]);
+
+  const { label: titleText, path } = title[index];
+
   return (
     <div>
       <header className={styles.header}>
         <div>
-          <img src={icon} alt="Button" />
+          <a href={path}>
+            <img src={icon} alt="Button" />
+          </a>
         </div>
-        <h1 className={styles.title}>Women</h1>
+        <h1 className={styles.title}>
+          <a className={styles.titleText} href={path}>
+            {titleText}
+          </a>
+        </h1>
       </header>
       <div className={styles.mainArrow}>
         <Arrow width="100%" height="100%" preserveAspectRatio="none" />
@@ -20,7 +46,7 @@ const Home = ({ user }) => {
       <div className={styles.subArrow}>
         <Arrow width="100%" height="100%" preserveAspectRatio="none" />
       </div>
-      <Slide />;
+      <Slide index={index} onNext={transit(1)} onPrev={transit(-1)} />;
     </div>
   );
 };
